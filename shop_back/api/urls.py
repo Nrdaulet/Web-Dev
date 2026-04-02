620eebd0ac4a5503815888c6538c2f572770d7e1
Nrdaulet <bazarbayev040@gmail.com>
2026-04-02 12:12:04 +0500

    lab 9

--- shop_back/api/urls.py
+++ shop_back/api/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
-from . import views
+# from django.urls import path
+# from . import views
 
-urlpatterns=[
-    path('products/', views.products_list),
-    path('products/<int:id>/', views.product_detail),
-    path('categories/', views.categories_list),
-    path('categories/<int:id>/', views.category_detail),
-    path('categories/<int:id>/products/', views.category_products),
-]
+# urlpatterns=[
+#     path('products/', views.products_list),
+#     path('products/<int:id>/', views.product_detail),
+#     path('categories/', views.categories_list),
+#     path('categories/<int:id>/', views.category_detail),
+#     path('categories/<int:id>/products/', views.category_products),
+# ]
+
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet,ProductViewSet
+
+router=DefaultRouter()
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+
+urlpatterns=router.urls
--- shop_back/api/views.py
+++ shop_back/api/views.py
@@ -1,38 +1,13 @@
-from django.shortcuts import render
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django.http import JsonResponse
 from .models import Product, Category
+from rest_framework import viewsets
+from .serializers import CategorySerializer, ProductSerializer
 
 def products_list(request):
-    products = Product.objects.all()
-
-    category = request.GET.get('category')
-    if category:
-        products = products.filter(category_id=category)
-
-   
-    active = request.GET.get('active')
-    if active:
-        if active.lower() == 'true':
-            products = products.filter(is_active=True)
-        elif active.lower() == 'false':
-            products = products.filter(is_active=False)
-
-    
-    search = request.GET.get('search')
-    if search:
-        products = products.filter(name__icontains=search)
-
-
-    sort = request.GET.get('sort')
-    if sort == 'price':
-        products = products.order_by('price')
-    elif sort == '-price':
-        products = products.order_by('-price')
-    elif sort == 'name':
-        products = products.order_by('name')
-
-
-    return JsonResponse(list(products.values()), safe=False, json_dumps_params={'indent': 2})
+    products = Product.objects.filter(price__gt=100000,price__lt=500000)
+    return JsonResponse(list(products.values()), safe=False, json_dumps_params={'indent':2})
 
 def product_detail(request, id):
     product = Product.objects.filter(id=id).values().first()
@@ -52,3 +27,17 @@ def category_detail(request, id):
 def category_products(request, id):
     products = Product.objects.filter(category_id=id).values()
     return JsonResponse(list(products), safe=False)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    @action(detail=True, methods=['get'])
+    def products(self, request, pk=None):
+        products = Product.objects.filter(category_id=pk)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset=Product.objects.all()
+    serializer_class=ProductSerializer
